acd780380c1f1e6353dd0d21ec41883748725fd2
tomh500 <95513888+tomh500@users.noreply.github.com>
2024-12-21 00:11:27 +0800

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,22 +1,21 @@
-// 获取弹窗var modal = document.getElementById("myModal");
-
-// 获取按钮，打开弹窗
-var btn document.getElementById("myBtn");
+ 获取弹窗
+ modal = document.getElementById("myModal");
 
 // 获取 <span> 元素，关闭弹窗
-var span = document.getElementsClassName("close")[];
+var span = document.getElementsByClassName("close")[0];
 
 // 获取音频元素
 var bgm = document.getElementById("bgm");
 
-// 当按钮时，打开弹窗
-btn.onclick = function() {
-    modal.style.display = "";
- 当点击 <span> (x 元素时，关闭弹窗并播放音频
-span.onclick = function()    modal.style.display =none";
-    bgm.play(); // 播放频
- 当弹窗外部时关闭弹窗并音频
+// 当页面加载时，打开弹窗
+window.onload = function() {
+    modal.style.display = "block"; // 显示弹窗
+    bgm.play(); // 播放音频
+ 当点击 <span>x 元素时，关闭弹窗并播放音频
+span.onclick = function() {
+    modal.style.display = "none";
+ 当点击弹窗外部时关闭弹窗
 window.onclick = function(event) {
- if (event.target modal) {
-        modal.style.display = "none";
-        bgm.play(); //放音频
+ if (event.target ==) {
+        modal.display = "none";
+ bgm.play(); 播放音频
